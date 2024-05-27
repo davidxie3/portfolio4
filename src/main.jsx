9185058128bf15fd1f3066e295311222e6197939
davidxie3 <davidxie04@gmail.com>
2024-05-27 11:15:03 -0400

    checkpoint

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
 import NewTheme from './styles/theme'
 
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 import NavBar from './components/navbar.jsx'
 import './styles/fonts.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider theme={NewTheme}>
       <NavBar/>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About(){
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Hi, I'm David Xie. I'm a software engineer and I love to code.</p>
+        </div>
+    );
+}
+
+
+export default About;
