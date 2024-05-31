822e8550284bc5cde2a1670f8c0557db3f77a4af
davidxie3 <davidxie04@gmail.com>
2024-05-30 21:46:50 -0400

    n

--- src/styles/components/navbar-style.jsx
+++ src/styles/components/navbar-style.jsx
@@ -1,17 +1,17 @@
-const NavBar =  {
-    // Styles for the base style
-    baseStyle: {},
-    // Styles for the size variations
-    sizes: {},
-    // Styles for the visual style variations
-    variants: {
-        aboutPage:{
-            color: 'black',
-            bg: 'mainPurple.secondaryBG'
-        }
-    },
-    // The default `size` or `variant` values
-    defaultProps: {},
-  }
+const NavBar = {
+	// Styles for the base style
+	baseStyle: {},
+	// Styles for the size variations
+	sizes: {},
+	// Styles for the visual style variations
+	variants: {
+		aboutPage: {
+			color: 'black',
+			bg: 'mainPurple.secondaryBG',
+		},
+	},
+	// The default `size` or `variant` values
+	defaultProps: {},
+};
 
 export default NavBar;
