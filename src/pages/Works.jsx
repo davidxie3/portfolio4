f801224f9bda2add02eb80383cf8352bb826b20e
davidxie3 <davidxie04@gmail.com>
2024-07-06 09:19:00 +0800

    WHO LET ME COOK. BANGED OUT MOST OF THE GRID IN ONE MORNING

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,15 +1,30 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, Grid, GridItem } from '@chakra-ui/react';
+
+import picture4 from '../assets/images/melcourses-mockup.webp';
 
 function Works() {
 	return (
-		<Flex h="calc(100vh)" display="flex" alignItems="center" justifyContent="center" flexDirection="column">
-			<Text
-				fontSize="4vw"
-				color="mainPurple.primary"
-				fontFamily="RomieRegular"
-			>
-				↙ Works ↘
-			</Text>
+		<Flex>
+			<Flex h="calc(200vh)" w="25vw" display="flex" direction="column" align="center" justify="start">
+				<Text
+					position="sticky"
+					fontSize="3.5vw"
+					color="mainPurple.primary"
+					fontFamily="RomieRegular"
+					top="150"
+					align="center"
+				>
+					↙SELECTED↘ WORKS
+				</Text>
+			</Flex>
+
+			<Flex>
+				<Grid mt="160" w="75vw" h='90%' templateColumns="repeat(2, 1fr)" gap={4}>
+					<GridItem bg="blue" />
+					<GridItem  bg="blue" />
+					<GridItem  bg="blue" />
+				</Grid>
+			</Flex>
 		</Flex>
 	);
 }
